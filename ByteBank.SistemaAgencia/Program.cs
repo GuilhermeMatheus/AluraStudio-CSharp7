7d223ea691354cf6313669ce11469b4f87b6c826
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-07-04 17:15:23 -0300

    Video 1.3

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,6 +12,34 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            // ARRAY de inteiros, com 5 posições!
+            int[] idades = null;
+            idades = new int[3];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            //idades[3] = 50;
+            //idades[4] = 28;
+            //idades[5] = 60;
+
+            Console.WriteLine(idades.Length);
+
+            int acumulador = 0;
+            for(int indice = 0; indice < idades.Length; indice++)
+            {
+                int idade = idades[indice];
+
+                Console.WriteLine($"Acessando o array idades no índice {indice}");
+                Console.WriteLine($"Valor de idades[{indice}] = {idade}");
+
+                acumulador += idade;
+            }
+
+            int media = acumulador / idades.Length;
+            Console.WriteLine($"Média de idades = {media}");
+
+
             Console.ReadLine();
         }
     }
